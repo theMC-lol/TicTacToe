2e09928fdb1bc21ed69c91e9e2d00fd34136d8b8
Arifi Alen <Arifi_Alen@Alens-MBP.fritz.box>
2019-11-25 18:18:03 +0100

    Methoden eingefügt

---
+++ TicTacToe/TicTacToe Kopie.java
@@ -0,0 +1,38 @@
+public class TicTacToe {
+
+	private int ROWS = 3;
+	private int COLS = 3;
+
+	public enum Player {
+		Unknown, X, O
+	}
+
+	public Player[][] board;
+	private Player currentPlayer;
+	private Player winner;
+
+	public TicTacToe() {
+	}
+	public void initialiazeBoard() {} 
+	
+	public boolean isGameWon() {
+
+	}
+
+	public boolean isDraw() {
+	}
+
+	public Player getWinner() {
+
+	}
+
+	public boolean isGameOver() {
+	}
+
+	public void play(int row, int col) {
+	}
+
+	public Player getCurrentPlayer() {
+		
+	}
+}
