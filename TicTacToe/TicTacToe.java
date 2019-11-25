154be40e9b1e71d7741a30881ba035ccd0ca4279
Tayfun Kan <tayfun@Tayfuns-MacBook-Pro.local>
2019-11-25 11:17:15 +0100

    Tayfunistdrin

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,4 +1,9 @@
-public static void main (String[]args)
-{
-    System.out.println("HalloWelt");
+class Main {
+    
+
+    public static void main (String[]args)
+    {
+        System.out.println("HalloWelt");
+    }   
+
 }
