db7d61b6b7cd38500e128856c51371d643dce3a6
Arifi Alen <Arifi_Alen@Alens-MBP.fritz.box>
2019-11-25 18:05:36 +0100

    Moin jungs

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -2,8 +2,9 @@ class Main {
     //Test Kommentar
     public static void main (String[]args)
     {
-        System.out.println("HalloWelt");
+        System.out.println("HalloWeltTayfunundManuundAlen");
 
-        
+
+        System.out.println("Hallo ich bins Alen");
     }   
 }
